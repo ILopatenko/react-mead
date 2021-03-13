377e45e878fe0be802fc1ffdfd7408935909159d
Iurii Lopatenko <yalopatenko@gmail.com>
2021-03-13 11:59:11 -0800

    017 - Manual data binding

--- ind-app/public/scripts/app.js
+++ ind-app/public/scripts/app.js
@@ -5,39 +5,54 @@ var count = 0;
 
 var addOne = function addOne() {
   console.log('addOne() is working ...');
+  count += 1;
+  console.log('Current value of count is ' + count);
+  renderCounter();
 };
+
 var minusOne = function minusOne() {
   console.log('minusOne() is working ...');
+  count -= 1;
+  console.log('Current value of count is ' + count);
+  renderCounter();
 };
+
 var reset = function reset() {
   console.log('reset() is working ...');
+  count = 0;
+  console.log('Current value of count is ' + count);
+  renderCounter();
 };
 
-var template016 = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Count: ',
-    count
-  ),
-  React.createElement(
-    'button',
-    { onClick: addOne },
-    ' +1 '
-  ),
-  React.createElement(
-    'button',
-    { onClick: reset },
-    ' reset '
-  ),
-  React.createElement(
-    'button',
-    { onClick: minusOne },
-    ' -1 '
-  )
-);
-
 var appRoot = document.getElementById('app');
-ReactDOM.render(template016, appRoot);
+
+var renderCounter = function renderCounter() {
+  var template016 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      ' +1 '
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      ' reset '
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      ' -1 '
+    )
+  );
+  ReactDOM.render(template016, appRoot);
+};
+
+renderCounter();
--- ind-app/src/app.js
+++ ind-app/src/app.js
@@ -3,22 +3,37 @@ let count = 0;
 
 const addOne = () => {
   console.log('addOne() is working ...');
+  count += 1;
+  console.log(`Current value of count is ${count}`);
+  renderCounter();
 };
+
 const minusOne = () => {
   console.log('minusOne() is working ...');
+  count -= 1;
+  console.log(`Current value of count is ${count}`);
+  renderCounter();
 };
+
 const reset = () => {
   console.log('reset() is working ...');
+  count = 0;
+  console.log(`Current value of count is ${count}`);
+  renderCounter();
 };
 
-const template016 = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}> +1 </button>
-    <button onClick={reset}> reset </button>
-    <button onClick={minusOne}> -1 </button>
-  </div>
-);
-
 const appRoot = document.getElementById('app');
-ReactDOM.render(template016, appRoot);
+
+const renderCounter = () => {
+  const template016 = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}> +1 </button>
+      <button onClick={reset}> reset </button>
+      <button onClick={minusOne}> -1 </button>
+    </div>
+  );
+  ReactDOM.render(template016, appRoot);
+};
+
+renderCounter();
