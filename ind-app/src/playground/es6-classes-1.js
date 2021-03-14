229a9ce6f48aa1bc5f766773af8fd0502c684aad
Iurii Lopatenko <yalopatenko@gmail.com>
2021-03-13 16:19:31 -0800

    025 - ES6 Classes (part 2)
    We have learned how to create a sub classes, how to change methods of parent class, how to add new methods

--- ind-app/src/playground/es6-classes-1.js
+++ ind-app/src/playground/es6-classes-1.js
@@ -1,18 +1,59 @@
+//Create a class Persone
 class Person {
   constructor(name = 'Anonymous', age = -1) {
     this.name = name;
     this.age = age;
   }
-  getGretting() {
+  getGreeting() {
     return `Hello, ${this.name}`;
   }
   getDescription() {
     return `I think that ${this.name} is ${this.age} year(s) old!`;
   }
 }
-
+console.log('PERSONS');
+//Create a new instance of class Person
 const me = new Person('Iurii Lopatenko', 34);
+//LOG result of executing method getDescription() of me
 console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getDescription());
+console.log('STUDENTS');
+class Student extends Person {
+  constructor(name, age, major) {
+    //add all the data from parent constructor
+    super(name, age);
+    //add additional properties
+    this.major = major;
+  }
+  //Add a new method
+  hasMajor() {
+    return !!this.major;
+  }
+  //Change a method from parent
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) description += ` Their major is ${this.major}`;
+    return description;
+  }
+}
+const meStudent = new Student('Iurii Lopatenko', 34, 'mining science');
+console.log(meStudent.getDescription());
+
+console.log('TEST TASK: TRAVELER');
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let greet = super.getGreeting();
+    if (!!this.homeLocation) {
+      greet += `. You are located at ${this.homeLocation}`;
+    }
+    return greet;
+  }
+}
+
+const testTraveler = new Traveler('Iurii Lopatenko', 34, 'Los Angeles');
+console.log(testTraveler.getGreeting());
